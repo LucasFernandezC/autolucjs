ac6152a9aa71e74efe2cf834f8798ad3600a8528
LucasFernandezC <lucas.fernandez.carbonell@gmail.com>
2022-06-29 22:10:48 -0300

    agregado de fetch selects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,26 @@
 //codigo central
 const arrayAutos = [];
-/*arrayAutos.push(new Auto("Ford", 2016, "Sedan", "4 ptas", "Diesel", true, true, false, "ford", 1));
-arrayAutos.push(new Auto("Ford", 2018, "Hatchback", "5 ptas", "Nafta", true, false, true, "ford", 2));
-arrayAutos.push(new Auto("VW", 2013, "Coupe", "3 ptas", "Nafta", true, true, false, "vw", 3));
-arrayAutos.push(new Auto("Fiat", 2010, "Hatchback", "5 ptas", "GNC", false, true, true, "fiat", 4));
-arrayAutos.push(new Auto("Fiat", 2012, "Sedan", "4 ptas", "Diesel", true, false, true, "fiat", 5));
-arrayAutos.push(new Auto("VW", 2016, "Hatchback", "5 ptas", "Nafta", true, false, true, "vw", 6));
-arrayAutos.push(new Auto("Ford", 2017, "Coupe", "3 ptas", "Nafta", true, false, true, "ford", 7));
-arrayAutos.push(new Auto("VW", 2018, "Hatchback", "5 ptas", "GNC", true, true, false, "vw", 8));
-arrayAutos.push(new Auto("VW", 2020, "Sedan", "4 ptas", "Diesel", true, false, true, "vw", 9));
-arrayAutos.push(new Auto("Fiat", 2010, "Hatchback", "5 ptas", "Nafta", true, false, true, "fiat", 10));
-arrayAutos.push(new Auto("Fiat", 2012, "Coupe", "3 ptas", "Nafta", true, true, false, "fiat", 11));
-arrayAutos.push(new Auto("Ford", 2013, "Hatchback", "5 ptas", "GNC", true, false, true, "ford", 12));
-arrayAutos.push(new Auto("Fiat", 2010, "Hatchback", "5 ptas", "Nafta", true, true, true, "fiat", 13));
-arrayAutos.push(new Auto("Fiat", 2012, "Coupe", "3 ptas", "Nafta", true, true, false, "fiat", 14));
-arrayAutos.push(new Auto("Ford", 2013, "Hatchback", "5 ptas", "GNC", true, true, true, "ford", 15));
-*/
-
 const arraySelects = [];
-arraySelects.push(new Selects("marca", "Todos"));
-arraySelects.push(new Selects("marca", "Ford"));
-arraySelects.push(new Selects("marca", "Fiat"));
-arraySelects.push(new Selects("marca", "VW"));
-arraySelects.push(new Selects("marca", "Toyota"));
-arraySelects.push(new Selects("tipo", "Todos"));
-arraySelects.push(new Selects("tipo", "Hatchback"));
-arraySelects.push(new Selects("tipo", "Sedan"));
-arraySelects.push(new Selects("tipo", "Camioneta"));
-arraySelects.push(new Selects("tipo", "Coupe"));
-arraySelects.push(new Selects("ptas", "Todos"));
-arraySelects.push(new Selects("ptas", "3 ptas"));
-arraySelects.push(new Selects("ptas", "4 ptas"));
-arraySelects.push(new Selects("ptas", "5 ptas"));
-arraySelects.push(new Selects("comb", "Todos"));
-arraySelects.push(new Selects("comb", "Nafta"));
-arraySelects.push(new Selects("comb", "Diesel"));
-arraySelects.push(new Selects("comb", "GNC"));
+
 
 fetch("./assets/js/autos.json")
-.then((respuesta)=>respuesta.json())
-.then((data)=>{
-    for (const elemento of data.autos) {
-        arrayAutos.push(new Auto(elemento.marca, elemento.anio, elemento.tipo, elemento.puertas, elemento.combustible, elemento.aire,elemento.esp,elemento.techo, elemento.img, elemento.id));
-    }
-    cargarPantalla(arrayAutos);
-})
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+        for (const elemento of data.autos) {
+            arrayAutos.push(new Auto(elemento.marca, elemento.anio, elemento.tipo, elemento.puertas, elemento.combustible, elemento.aire, elemento.esp, elemento.techo, elemento.img, elemento.id));
+        }
+        cargarPantalla(arrayAutos);
+        fetch("./assets/js/select.json")
+            .then((respuesta) => respuesta.json())
+            .then((data) => {
+                for (const elemento of data.selects) {
+                    arraySelects.push(new Selects(elemento.tipo, elemento.valor));
+                }
+                cargarSelects(arrayAutos);;
+            })
+    })
+
+
 
 let carrito = [];
 let carritoGrabado = JSON.parse(localStorage.getItem("listacarr"));
@@ -61,7 +37,7 @@ botonReset.onclick = () => { cargarPantalla(arrayAutos); cargarSelects(arrayAuto
 
 //funciones
 //esta funcion se encarga de volver a generar el carrito a partir de lo que recupero del storage
-function acomodarCarrito(recuperado){
+function acomodarCarrito(recuperado) {
     for (const literal of recuperado) {
         let temp = new Auto(literal.marca, literal.anio, literal.tipo, literal.puertas, literal.combustible, literal.aire, literal.esp, literal.techo, literal.img, literal.id);
         carrito.push(temp);
@@ -72,33 +48,32 @@ function acomodarCarrito(recuperado){
 
 //funcion que agrega productos al array del carrito
 function agregarCarrito(e) {
-    
-    if (carrito.some((el)=> el.id == e.id))
-    {
+
+    if (carrito.some((el) => el.id == e.id)) {
         Swal.fire(
             'El vehiculo ya se encuentra en favoritos',
             'se dejo en favoritos',
             'info'
-          )
+        )
     }
     else {
         carrito.push(e);
         Toastify({
             text: "Auto agregado!",
             duration: 3000
-            }).showToast();
+        }).showToast();
         mostrarCarrito(0);
     }
 
 }
 
 function sacarCarrito(e) {
-    
-    carrito=carrito.filter((el) => e.id != el.id);
+
+    carrito = carrito.filter((el) => e.id != el.id);
     Toastify({
         text: "Auto olvidado!",
         duration: 3000
-        }).showToast();
+    }).showToast();
     mostrarCarrito();
 
 
@@ -107,26 +82,26 @@ function sacarCarrito(e) {
 //funcion que edita el html para agregar los productos al carrito
 function mostrarCarrito(ejecucion) {
     let carritoDom = document.getElementById("carrito");
-    carritoDom.innerHTML="";
+    carritoDom.innerHTML = "";
     for (const item of carrito) {
-        
+
         li = document.createElement("li");
         button = document.createElement("button");
         button.class = "dropdown-item";
         button.innerText = item.marca + item.anio;
         let carritoButton = document.createElement("a");
         carritoButton.className = "btn btn-primary center";
-        carritoButton.id = "btnrmv"+item.id;
+        carritoButton.id = "btnrmv" + item.id;
         carritoButton.text = "No me gusta!";
         li.appendChild(button);
         li.appendChild(carritoButton);
         carritoDom.appendChild(li);
 
     }
-    ejecucion != 1 ? localStorage.setItem("listacarr",JSON.stringify(carrito)) : "";
-    carrito.forEach(auto =>{document.getElementById("btnrmv"+auto.id).addEventListener("click", function () { sacarCarrito(auto) })});
-    
-    
+    ejecucion != 1 ? localStorage.setItem("listacarr", JSON.stringify(carrito)) : "";
+    carrito.forEach(auto => { document.getElementById("btnrmv" + auto.id).addEventListener("click", function () { sacarCarrito(auto) }) });
+
+
 }
 
 //Funcion que genera el html con los vehiculos. se utiliza tanto para la carga inicial como para cuando se aplican filtros
@@ -152,7 +127,7 @@ function cargarPantalla(array) {
         let detalle = autos.puertas + ", " + autos.combustible + ", " + autos.tipo;
         autos.aire == true && (detalle += ", aire acondicionado");
         autos.esp && (detalle += ", Control de estabilidad");
-        autos.techo && (detalle += ", Techo corredizo" );
+        autos.techo && (detalle += ", Techo corredizo");
         cardP.innerText = detalle;
         let cardButton = document.createElement("a");
         cardButton.className = "btn btn-primary center";
@@ -248,10 +223,10 @@ function filtrarBusqueda() {
             'La busqueda no arrojo resultados',
             'se cargaran los autos nuevamente',
             'info'
-          );
+        );
         cargarPantalla(arrayAutos);
         cargarSelects(arrayAutos);
-        
+
 
     }
     else {
